415cd7f2f063095c293f558558f71ff1701ed132
HunchakAndrii <andreygunchak1@gmail.com>
2023-10-16 23:58:16 +0300

    start financial

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 console.log('Hello Dima')
+
+document.querySelector('.burger').addEventListener('click', (e) => {
+  e.preventDefault()
+  document.querySelector('.burger').classList.toggle('active')
+})
+
+let titleHeight = 0
+
+document.querySelectorAll('.info-block__title').forEach(item => {
+  if(item.scrollHeight > titleHeight) {
+    titleHeight = item.scrollHeight
+  }
+})
+
+document.querySelectorAll('.info-block__title').forEach(item => {
+  item.style.height = titleHeight + 'px'
+})
