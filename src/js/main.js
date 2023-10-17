ae120f33ae49bef47709f8ab3ecababf1621f01b
HunchakAndrii <andreygunchak1@gmail.com>
2023-10-17 17:21:50 +0300

    17.10.23

--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,14 @@ document.querySelector('.burger').addEventListener('click', (e) => {
   document.querySelector('.burger').classList.toggle('active')
 })
 
-let titleHeight = 0
+// let titleHeight = 0
 
-document.querySelectorAll('.info-block__title').forEach(item => {
-  if(item.scrollHeight > titleHeight) {
-    titleHeight = item.scrollHeight
-  }
-})
+// document.querySelectorAll('.info-block__title').forEach(item => {
+//   if(item.scrollHeight > titleHeight) {
+//     titleHeight = item.scrollHeight
+//   }
+// })
 
-document.querySelectorAll('.info-block__title').forEach(item => {
-  item.style.height = titleHeight + 'px'
-})
+// document.querySelectorAll('.info-block__title').forEach(item => {
+//   item.style.height = titleHeight + 'px'
+// })
